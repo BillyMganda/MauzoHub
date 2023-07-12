45d8eb1d69a942d1e124ae1c473ca89709526628
Billy <bimganda@gmail.com>
2023-07-11 20:36:52 -0400

    add base entity class

---
+++ MauzoHub.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
