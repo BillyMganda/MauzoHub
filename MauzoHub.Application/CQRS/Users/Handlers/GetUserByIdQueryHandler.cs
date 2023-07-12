50c44d227c36fb7e02ee9c86dfebc7c0254154a0
Billy <bimganda@gmail.com>
2023-07-12 06:56:09 -0400

    create cqrs query handler class to get a user by id

---
+++ MauzoHub.Application/CQRS/Users/Handlers/GetUserByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MauzoHub.Application.CQRS.Users.Queries;
+using MauzoHub.Application.DTOs;
+using MauzoHub.Domain.Interfaces;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Users.Handlers
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, GetUserDto>
+    {
+        private readonly IUserRepository _userRepository;
+        public GetUserByIdQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetUserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            var userDto = new GetUserDto()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+            };
+
+            return userDto;
+        }
+    }
+}
