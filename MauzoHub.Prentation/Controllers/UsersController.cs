c2f3fd0860394ef0eb75d2be802339913ba3a6ec
Billy <bimganda@gmail.com>
2023-07-12 06:58:21 -0400

    update controller, add method to get user by id

--- MauzoHub.Prentation/Controllers/UsersController.cs
+++ MauzoHub.Prentation/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using MauzoHub.Application.CQRS.Users.Commands;
+using MauzoHub.Application.CQRS.Users.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,12 +26,20 @@ namespace MauzoHub.Prentation.Controllers
             try
             {
                 var userDto = await _mediator.Send(command);
-                return CreatedAtAction(nameof(GetUser), new { id = userDto.Id }, userDto);
+                return CreatedAtAction(nameof(GetUserById), new { id = userDto.Id }, userDto);
             }
             catch (Exception)            {
                 
                 return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the user.");
             }
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            var query = new GetUserByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
         }
     }
 }
