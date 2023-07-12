c521e2730d902eebe45af26bc2c091f2622e9d76
Billy <bimganda@gmail.com>
2023-07-12 06:21:34 -0400

    create a cqrs command class for creating a user

---
+++ MauzoHub.Application/CQRS/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Users.Commands
+{
+    public class CreateUserCommand : IRequest<GetUserDto>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
