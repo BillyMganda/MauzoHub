c0cfcb1ef075abcbd97133292ea8f83fb1d81249
Billy <bimganda@gmail.com>
2023-07-12 06:50:31 -0400

    add controller with method to post user data

---
+++ MauzoHub.Prentation/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using MauzoHub.Application.CQRS.Users.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauzoHub.Prentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var userDto = await _mediator.Send(command);
+                return CreatedAtAction(nameof(GetUser), new { id = userDto.Id }, userDto);
+            }
+            catch (Exception)            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the user.");
+            }
+        }
+    }
+}
