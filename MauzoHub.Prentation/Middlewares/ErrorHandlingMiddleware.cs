c424f0c90d82882e90bddd49808291414db868fc
Billy <bimganda@gmail.com>
2023-07-13 04:18:12 -0400

    create middleware class that will handle all other exceptions later

---
+++ MauzoHub.Prentation/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+namespace MauzoHub.Prentation.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            var message = "An unexpected error occurred.";
+
+            if (ex is BadRequestException)
+            {
+                statusCode = HttpStatusCode.BadRequest;
+                message = ex.Message;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsJsonAsync(new { error = message });
+        }
+    }
+}
