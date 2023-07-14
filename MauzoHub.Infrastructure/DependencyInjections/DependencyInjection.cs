1c85a953753398124bb149ede8b0e9b9d76b7d8d
Billy <bimganda@gmail.com>
2023-07-14 12:32:47 -0400

    add redis connection to dependency injection

--- MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
+++ MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
@@ -3,7 +3,7 @@ using MauzoHub.Infrastructure.Databases;
 using MauzoHub.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using StackExchange.Redis;
 
 namespace MauzoHub.Infrastructure.DependencyInjections
 {
@@ -18,7 +18,23 @@ namespace MauzoHub.Infrastructure.DependencyInjections
                 options.DatabaseName = configuration.GetSection("MauzoHubDatabase:DatabaseName").Value!;
                 options.UsersCollectionName = configuration.GetSection("MauzoHubDatabase:UsersCollectionName").Value!;
                 // Set other properties of MauzoHubDatabaseSettings here
-            });                        
+            });
+
+            // Redis Injection
+            services.AddStackExchangeRedisCache(options =>
+            {
+                var redisSettings = configuration.GetSection("RedisSettings");
+                var connectionString = configuration.GetSection("RedisSettings:ConnectionString").Value!;
+
+                options.Configuration = connectionString;
+            });
+            services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var redisSettings = configuration.GetSection("RedisSettings");
+                var connectionString = configuration.GetSection("RedisSettings:ConnectionString").Value!;
+
+                return ConnectionMultiplexer.Connect(connectionString);
+            });
 
             // Services Injection
             services.AddScoped<IUserRepository, UserRepository>();
