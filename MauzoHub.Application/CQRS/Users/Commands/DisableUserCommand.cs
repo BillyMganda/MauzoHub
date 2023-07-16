f61e5df815234d716cfa0442952f661155c2a5c8
Billy <bimganda@gmail.com>
2023-07-16 05:00:03 -0400

    add command class to disable a user by id

---
+++ MauzoHub.Application/CQRS/Users/Commands/DisableUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Users.Commands
+{
+    public class DisableUserCommand : IRequest<GetUserDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
