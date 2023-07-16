09f646df679aa8637e3797fd587ae6e24f466a7d
Billy <bimganda@gmail.com>
2023-07-16 06:41:07 -0400

    create ioauth repository interface class

---
+++ MauzoHub.Domain/Interfaces/IOAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MauzoHub.Domain.Interfaces
+{
+    public interface IOAuthRepository<T, V, W, X, Y, Z> where T : class
+    {
+        Task<T> LoginAsync(V LoginRequest);
+        Task<T> RefreshTokenAsync(W RefreshTokenRequest);
+        Task<T> LogoutAsync(X LogoutRequest);
+        Task<T> RememberPasswordAsync(Y RememberPasswordRequest);
+        Task<T> ResetPasswordAsync(Z ResetPasswordRequest);
+    }
+}
