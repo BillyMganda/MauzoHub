37cb06f85f2abbb8efa9c920ad630ded3b57b041
Billy <bimganda@gmail.com>
2023-07-16 14:14:17 -0400

    create OAuthRepository class that implement IOAuthRepository interface

---
+++ MauzoHub.Infrastructure/Repositories/OAuthRepository.cs
@@ -0,0 +1,37 @@
+﻿using MauzoHub.Domain.Interfaces;
+
+namespace MauzoHub.Infrastructure.Repositories
+{
+    public class OAuthRepository<T, V, W, X, Y, Z> : IOAuthRepository<T, V, W, X, Y, Z> where T : class
+    {
+        private readonly IUserRepository _userRepository;
+        public OAuthRepository(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public Task<T> LoginAsync(V LoginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> LogoutAsync(X LogoutRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RefreshTokenAsync(W RefreshTokenRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RememberPasswordAsync(Y RememberPasswordRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> ResetPasswordAsync(Z ResetPasswordRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
