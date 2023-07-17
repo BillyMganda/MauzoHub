6ef50f48e8b4b5a608182034c0453012f4c63e78
Billy <bimganda@gmail.com>
2023-07-17 15:57:43 -0400

    add user refresh tokens class

---
+++ MauzoHub.Domain/DTOs/UserRefreshTokens.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Domain.DTOs
+{
+    public class UserRefreshTokens
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
