41a39b0e7c26dab475177c0b6cca9a324785e4c5
Billy <bimganda@gmail.com>
2023-07-17 15:55:51 -0400

    add tokens class

---
+++ MauzoHub.Domain/DTOs/Tokens.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Domain.DTOs
+{
+    public class Tokens
+    {
+        public string Access_Token { get; set; } = string.Empty;
+        public string Refresh_Token { get; set; } = string.Empty;
+    }
+}
