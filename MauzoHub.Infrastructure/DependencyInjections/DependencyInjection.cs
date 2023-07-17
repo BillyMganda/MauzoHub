48003a6cc1ddd25fd5dd864be814bf5be853c3eb
Billy <bimganda@gmail.com>
2023-07-17 13:00:07 -0400

    add jwt to infa layer dependency injection

--- MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
+++ MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using MauzoHub.Domain.Interfaces;
 using MauzoHub.Infrastructure.Databases;
 using MauzoHub.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
+using System.Text;
 
 namespace MauzoHub.Infrastructure.DependencyInjections
 {
@@ -39,6 +42,18 @@ namespace MauzoHub.Infrastructure.DependencyInjections
             // Services Injection
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IRedisCacheProvider, RedisCacheProvider>();
+
+            // JWT
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Token").Value!)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
         }
     }
 }
