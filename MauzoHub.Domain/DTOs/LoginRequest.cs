6bc71049989a5f1ac00b79187defdba02129f036
Billy <bimganda@gmail.com>
2023-07-17 15:54:39 -0400

    add login request class

---
+++ MauzoHub.Domain/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Domain.DTOs
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
