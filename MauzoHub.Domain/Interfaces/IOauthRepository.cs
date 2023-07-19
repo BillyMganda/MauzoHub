c9e8c4a66226dda948c93e84e4dcfacbf7ba5235
Billy <bimganda@gmail.com>
2023-07-19 16:18:41 -0400

    implement iouth interface class, to add more features later

--- MauzoHub.Domain/Interfaces/IOauthRepository.cs
+++ MauzoHub.Domain/Interfaces/IOauthRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace MauzoHub.Domain.Interfaces
+﻿using MauzoHub.Domain.DTOs;
+
+namespace MauzoHub.Domain.Interfaces
 {
     public interface IOauthRepository
     {
-        void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt);
+        void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt);        
+        bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+        string CreateJwtToken(LoginRequest request);
     }
 }
---
+++ MauzoHub.Infrastructure/Repositories/OauthRepository.cs
@@ -0,0 +1,59 @@
+﻿using MauzoHub.Domain.DTOs;
+using MauzoHub.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MauzoHub.Infrastructure.Repositories
+{
+    public class OauthRepository : IOauthRepository    {
+        
+        private readonly IConfiguration _configuration;
+        public OauthRepository(IConfiguration configuration)
+        {
+            
+            _configuration = configuration;
+        }
+
+        public void CreatePasswordHash(string password, out string passwordHash, out string passwordSalt)
+        {
+            using (var hmac = new HMACSHA256())
+            {
+                passwordSalt = Convert.ToBase64String(hmac.Key);
+                var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                passwordHash = Convert.ToBase64String(hashBytes);
+            }
+        }                       
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {            
+            using (var hmac = new HMACSHA256(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        public string CreateJwtToken(LoginRequest request)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, request.Email)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("JWT:Key").Value!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken
+                (
+                claims: claims,
+                expires: DateTime.Now.AddHours(5),
+                signingCredentials: creds
+                );
+
+            string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwtToken;
+        }
+    }
+}
