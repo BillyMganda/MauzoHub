d80c20ba9d399403cfa1bf48b0522174f219fa37
Billy <bimganda@gmail.com>
2023-07-24 14:54:11 -0400

    create a login response class

---
+++ MauzoHub.Domain/Entities/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
