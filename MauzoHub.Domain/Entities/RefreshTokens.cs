4d366240c4c31f445d153c3510c268689dae30b6
Billy <bimganda@gmail.com>
2023-07-24 14:45:18 -0400

    create a refresh tokens class in domain layer

---
+++ MauzoHub.Domain/Entities/RefreshTokens.cs
@@ -0,0 +1,12 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class RefreshTokens
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiryDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
