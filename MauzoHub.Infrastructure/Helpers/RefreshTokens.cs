deb90cd96413847b77785abb20db3db41cf3810a
Billy <bimganda@gmail.com>
2023-07-24 14:17:18 -0400

    create a refresh token class

---
+++ MauzoHub.Infrastructure/Helpers/RefreshTokens.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Infrastructure.Helpers
+{
+    public class RefreshTokens
+    {
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
