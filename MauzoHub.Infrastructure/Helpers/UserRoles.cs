97f118f04f65df38199bc2714014a512802bb938
Billy <bimganda@gmail.com>
2023-07-24 14:23:22 -0400

    add a class containing roles

---
+++ MauzoHub.Infrastructure/Helpers/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace MauzoHub.Infrastructure.Helpers
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Business = "Business";
+        public const string User = "User";
+    }
+}
