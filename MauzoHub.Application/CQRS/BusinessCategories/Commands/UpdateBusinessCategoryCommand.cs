ee972685574876d21de0576ca4d95fdf67d0e756
Billy <bimganda@gmail.com>
2023-07-25 17:00:33 -0400

    add crqs command class to update a category

---
+++ MauzoHub.Application/CQRS/BusinessCategories/Commands/UpdateBusinessCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.BusinessCategories.Commands
+{
+    public class UpdateBusinessCategoryCommand : IRequest<GetCategoryDto>
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
