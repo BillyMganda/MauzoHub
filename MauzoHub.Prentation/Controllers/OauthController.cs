07ea94ade95eb8bcce7bfb19dad7532662a9135a
Billy <bimganda@gmail.com>
2023-07-25 11:58:59 -0400

    rename return value in login controller method

--- MauzoHub.Prentation/Controllers/OauthController.cs
+++ MauzoHub.Prentation/Controllers/OauthController.cs
@@ -17,8 +17,8 @@ namespace MauzoHub.Prentation.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginCommand command)
         {
-            var Token = await _mediator.Send(command);
-            return Ok(Token);  
+            var Response = await _mediator.Send(command);
+            return Ok(Response);  
         }
 
         [HttpPost("forgot-password")]
