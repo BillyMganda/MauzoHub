c7115388e856b6674e4b0764bb0cf7cd3264120b
Billy <bimganda@gmail.com>
2023-07-26 15:10:26 -0400

    add cqrs command class to delete a business

---
+++ MauzoHub.Application/CQRS/Businesses/Commands/DeleteBusinessCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Businesses.Commands
+{
+    public class DeleteBusinessCommand : IRequest<Unit>
+    {
+        [Required]        
+        public Guid Id { get; set; }
+    }
+}
