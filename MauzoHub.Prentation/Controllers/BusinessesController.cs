43de0c2d25158671d86520ee8282ceb36935460e
Billy <bimganda@gmail.com>
2023-07-26 07:42:07 -0400

    add businesses controller with method to add a business

---
+++ MauzoHub.Prentation/Controllers/BusinessesController.cs
@@ -0,0 +1,34 @@
+﻿using MauzoHub.Application.CQRS.BusinessCategories.Commands;
+using MauzoHub.Application.CQRS.BusinessCategories.Queries;
+using MauzoHub.Application.CQRS.Businesses.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauzoHub.Prentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public BusinessesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBusiness([FromBody] AddBusinessCommand command)
+        {
+            var businessDto = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetBusinessById), new { id = businessDto.Id }, businessDto);
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<IActionResult> GetBusinessById(Guid id)
+        {
+            var query = new GetCategoryByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
