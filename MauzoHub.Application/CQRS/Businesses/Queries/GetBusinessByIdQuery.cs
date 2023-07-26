53daa6fc052e2ba378ba3b4241825c25acb9e4de
Billy <bimganda@gmail.com>
2023-07-26 12:10:47 -0400

    add cqrs query class to get business by id

---
+++ MauzoHub.Application/CQRS/Businesses/Queries/GetBusinessByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Businesses.Queries
+{
+    public class GetBusinessByIdQuery : IRequest<GetBusinessDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
