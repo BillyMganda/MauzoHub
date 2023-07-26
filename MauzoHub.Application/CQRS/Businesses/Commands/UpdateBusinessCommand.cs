de692efa7ab248db03dd16789123e355fcc05e18
Billy <bimganda@gmail.com>
2023-07-26 14:49:27 -0400

    add cqrs command to update a business

---
+++ MauzoHub.Application/CQRS/Businesses/Commands/UpdateBusinessCommand.cs
@@ -0,0 +1,18 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Businesses.Commands
+{
+    public class UpdateBusinessCommand : IRequest<GetBusinessDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required, MinLength(500)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public Guid CategoryId { get; set; }
+    }
+}
