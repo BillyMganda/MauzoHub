81474789693637b45dc40907cdb6615fc276ec0b
Billy <bimganda@gmail.com>
2023-07-26 06:19:05 -0400

    inject BusinessCollectionName to db settings in DI container in infra layer

--- MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
+++ MauzoHub.Infrastructure/DependencyInjections/DependencyInjection.cs
@@ -22,6 +22,7 @@ namespace MauzoHub.Infrastructure.DependencyInjections
                 options.UsersCollectionName = configuration.GetSection("MauzoHubDatabase:UsersCollectionName").Value!;
                 options.RefreshTokensCollectionName = configuration.GetSection("MauzoHubDatabase:RefreshTokensCollectionName").Value!;
                 options.BusinessCategoriesCollectionName = configuration.GetSection("MauzoHubDatabase:BusinessCategoriesCollectionName").Value!;
+                options.BusinessCollectionName = configuration.GetSection("MauzoHubDatabase:BusinessCollectionName").Value!;
                 // Set other properties of MauzoHubDatabaseSettings here
             });
 
