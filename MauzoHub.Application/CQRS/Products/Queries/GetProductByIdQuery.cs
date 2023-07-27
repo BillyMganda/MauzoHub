84f7405585e4b5110b4bfc17d3349daa7b1cdba7
Billy <bimganda@gmail.com>
2023-07-27 20:53:24 +0300

    create a cqrs query to get a product by id

---
+++ MauzoHub.Application/CQRS/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<GetProductsDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
+
