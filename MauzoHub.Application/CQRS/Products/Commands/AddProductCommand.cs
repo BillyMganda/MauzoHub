de98f874614a2e2d64d7435f89349cd0d579117c
Billy <bimganda@gmail.com>
2023-07-27 06:40:31 -0400

    create a cqrs command class to add a product

---
+++ MauzoHub.Application/CQRS/Products/Commands/AddProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Products.Commands
+{
+    public class AddProductCommand : IRequest<GetProductsDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> Images { get; set; } = new List<string>();
+        public Guid BusinessId { get; set; }
+    }
+}
