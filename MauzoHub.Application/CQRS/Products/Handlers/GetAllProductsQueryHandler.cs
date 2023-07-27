f93542c207ceec16b53004944f64805ac024f043
Billy <bimganda@gmail.com>
2023-07-27 21:29:05 +0300

    add GetAllProductsQueryHandler, a cqrs query handler class to get all products

---
+++ MauzoHub.Application/CQRS/Products/Handlers/GetAllProductsQueryHandler.cs
@@ -0,0 +1,59 @@
+﻿using MauzoHub.Application.CQRS.Products.Queries;
+using MauzoHub.Application.DTOs;
+using MauzoHub.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace MauzoHub.Application.CQRS.Products.Handlers
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<GetProductsDto>>
+    {
+        private readonly IProductsRepository _productRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public GetAllProductsQueryHandler(IProductsRepository productRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            _productRepository = productRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IEnumerable<GetProductsDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var remoteIpAddress = httpContext.Connection.RemoteIpAddress;
+
+            var actionUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}{httpContext.Request.Path}";
+            var httpMethod = httpContext.Request.Method;
+
+            try
+            {
+                var products = await _productRepository.GetAllAsync();
+
+                var productsDto = products.Select(p => new GetProductsDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Images = p.Images,
+                    BusinessId = p.BusinessId,
+                });
+
+                return productsDto;
+            }
+            catch (Exception ex)
+            {
+                var errorLog = new ErrorLog
+                {
+                    DateTime = DateTime.Now,
+                    ErrorCode = "500",
+                    ErrorMessage = ex.Message,
+                    IPAddress = remoteIpAddress.ToString(),
+                    ActionUrl = actionUrl,
+                    HttpMethod = httpMethod,
+                };
+                Log.Error(ex, "An error occurred while processing the command: {@ErrorLog}", errorLog);
+                throw;
+            }
+        }
+    }
+}
