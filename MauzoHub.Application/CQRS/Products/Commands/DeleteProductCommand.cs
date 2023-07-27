23328b54f662c6cb8c373fc4f2dd9085dccb5e38
Billy <bimganda@gmail.com>
2023-07-27 22:19:39 +0300

    add cqrs command to delete a product

---
+++ MauzoHub.Application/CQRS/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Products.Commands
+{
+    public class DeleteProductCommand : IRequest<Unit>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
