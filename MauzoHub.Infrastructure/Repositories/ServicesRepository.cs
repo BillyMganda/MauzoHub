bf25facf321e9b7efff983942f8b97b7ee37b4fc
Billy <bimganda@gmail.com>
2023-07-28 15:32:28 +0300

    implement IServicesRepository, Inject the service to DI container, create db connection to the collection

---
+++ MauzoHub.Infrastructure/Repositories/ServicesRepository.cs
@@ -0,0 +1,23 @@
+﻿using MauzoHub.Domain.Entities;
+using MauzoHub.Domain.Interfaces;
+using MauzoHub.Infrastructure.Databases;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace MauzoHub.Infrastructure.Repositories
+{
+    public class ServicesRepository : IServicesRepository
+    {
+        private readonly IMongoCollection<Services> _servicesCollection;
+        private readonly IRedisCacheProvider _redisCache;
+        public ServicesRepository(IOptions<MauzoHubDatabaseSettings> databaseSettings, IRedisCacheProvider redisCache)
+        {
+            var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+            var database = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+
+            _servicesCollection = database.GetCollection<Services>(databaseSettings.Value.ServicesCollectionName);
+
+            _redisCache = redisCache;
+        }
+    }
+}
