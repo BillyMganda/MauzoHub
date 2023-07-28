262924c46f0e1952d62c88061cc4b9a22d6afc2c
Billy <bimganda@gmail.com>
2023-07-28 16:52:16 +0300

    create command to update a service

---
+++ MauzoHub.Application/CQRS/Services/Commands/UpdateServiceCommand.cs
@@ -0,0 +1,16 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Services.Commands
+{
+    public class UpdateServiceCommand : IRequest<GetServiceDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(500)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
