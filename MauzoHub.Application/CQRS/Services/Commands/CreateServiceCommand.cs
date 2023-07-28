be780e1a2e431062db146c48055006f430fe439d
Billy <bimganda@gmail.com>
2023-07-28 16:12:37 +0300

    create a cqrs command class to create a new service

---
+++ MauzoHub.Application/CQRS/Services/Commands/CreateServiceCommand.cs
@@ -0,0 +1,14 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace MauzoHub.Application.CQRS.Services.Commands
+{
+    public class CreateServiceCommand : IRequest<GetServiceDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<IFormFile> Images { get; set; } = new List<IFormFile>();
+        public Guid BusinessId { get; set; }
+    }
+}
