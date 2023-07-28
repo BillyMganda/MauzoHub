1c44d5b11b3d9ce4b4e31bff556f7edbcf480361
Billy <bimganda@gmail.com>
2023-07-28 22:02:10 +0300

    create command handler class to create an appointment

---
+++ MauzoHub.Application/CQRS/Appointments/Handlers/CreateAppointmentCommandHandler.cs
@@ -0,0 +1,72 @@
+﻿using MauzoHub.Application.CQRS.Appointments.Commands;
+using MauzoHub.Application.CustomExceptions;
+using MauzoHub.Application.DTOs;
+using MauzoHub.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace MauzoHub.Application.CQRS.Appointments.Handlers
+{
+    public class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, GetAppointmentDto>
+    {
+        private readonly IAppointmentRepository _appointmentsRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CreateAppointmentCommandHandler(IAppointmentRepository appointmentsRepository, IHttpContextAccessor contextAccessor)
+        {
+            _appointmentsRepository = appointmentsRepository;
+            _httpContextAccessor = contextAccessor;
+        }
+
+        public async Task<GetAppointmentDto> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var remoteIpAddress = httpContext.Connection.RemoteIpAddress;
+
+            var actionUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}{httpContext.Request.Path}";
+            var httpMethod = httpContext.Request.Method;
+
+            try
+            {
+                var appointment = new Domain.Entities.Appointments
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = DateTime.Now,
+                    LastModified = DateTime.Now,
+                    UserId = request.UserId,
+                    ServiceId = request.ServiceId,
+                    AppointmentDate = request.AppointmentDate,
+                    AppointmentTime = request.AppointmentTime,
+                };
+
+                await _appointmentsRepository.AddAsync(appointment);
+
+                var apointmentDto = new GetAppointmentDto
+                {
+                    Id = appointment.Id,
+                    UserId = appointment.UserId,
+                    ServiceId = appointment.ServiceId,
+                    AppointmentDate = appointment.AppointmentDate,
+                    AppointmentTime = appointment.AppointmentTime,
+                };
+
+                return apointmentDto;
+            }
+            catch (Exception ex)
+            {
+                var errorLog = new ErrorLog
+                {
+                    DateTime = DateTime.Now,
+                    ErrorCode = "500",
+                    ErrorMessage = ex.Message,
+                    IPAddress = remoteIpAddress.ToString(),
+                    ActionUrl = actionUrl,
+                    HttpMethod = httpMethod,
+                };
+                Log.Error(ex, "An error occurred while processing the command: {@ErrorLog}", errorLog);
+
+                throw;
+            }
+        }
+    }
+}
