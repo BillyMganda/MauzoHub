abf22d692ead24c3e778224253d4849511a63992
Billy <bimganda@gmail.com>
2023-07-28 17:27:21 +0300

    create query class to get a service by id

---
+++ MauzoHub.Application/CQRS/Services/Queries/GetServiceByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Services.Queries
+{
+    public class GetServiceByIdQuery : IRequest<GetServiceDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
