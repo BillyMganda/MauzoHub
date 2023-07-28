46d57f0680fc4ec5170de594fc3f416ceddeec0e
Billy <bimganda@gmail.com>
2023-07-28 16:46:02 +0300

    create command handler class for creating a service.
    create command class for deleting a service

---
+++ MauzoHub.Application/CQRS/Services/Commands/DeleteServiceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace MauzoHub.Application.CQRS.Services.Commands
+{
+    public class DeleteServiceCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ MauzoHub.Application/CQRS/Services/Handlers/CreateServiceCommandHandler.cs
@@ -0,0 +1,85 @@
+﻿using MauzoHub.Application.CQRS.Services.Commands;
+using MauzoHub.Application.DTOs;
+using MauzoHub.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace MauzoHub.Application.CQRS.Services.Handlers
+{
+    public class CreateServiceCommandHandler : IRequestHandler<CreateServiceCommand, GetServiceDto>
+    {
+        private readonly IServicesRepository _servicesRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CreateServiceCommandHandler(IServicesRepository servicesRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            _servicesRepository = servicesRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<GetServiceDto> Handle(CreateServiceCommand request, CancellationToken cancellationToken)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var remoteIpAddress = httpContext.Connection.RemoteIpAddress;
+
+            var actionUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}{httpContext.Request.Path}";
+            var httpMethod = httpContext.Request.Method;
+
+            try
+            {
+                // Handle image uploads and convert them to strings
+                var imageStrings = new List<string>();
+                foreach (var imageFile in request.Images)
+                {
+                    if (imageFile.Length > 0)
+                    {
+                        using var memoryStream = new MemoryStream();
+                        await imageFile.CopyToAsync(memoryStream);
+                        imageStrings.Add(Convert.ToBase64String(memoryStream.ToArray()));
+                    }
+                }
+
+
+                var newService = new Domain.Entities.Services
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = DateTime.Now,
+                    LastModified = DateTime.Now,
+                    Name = request.Name,
+                    Description = request.Description,
+                    Images = imageStrings,
+                    BusinessId = request.BusinessId,
+                };
+
+                await _servicesRepository.AddAsync(newService);
+
+                var serviceDto = new GetServiceDto
+                {
+                    Id = newService.Id,
+                    Name = newService.Name,
+                    Description = newService.Description,
+                    Images = newService.Images,
+                    BusinessId = newService.BusinessId,
+                };
+
+                return serviceDto;
+
+            }
+            catch (Exception ex)
+            {
+                var errorLog = new ErrorLog
+                {
+                    DateTime = DateTime.Now,
+                    ErrorCode = "500",
+                    ErrorMessage = ex.Message,
+                    IPAddress = remoteIpAddress.ToString(),
+                    ActionUrl = actionUrl,
+                    HttpMethod = httpMethod,
+                };
+                Log.Error(ex, "An error occurred while processing the command: {@ErrorLog}", errorLog);
+
+                throw;
+            }
+        }
+    }
+}
