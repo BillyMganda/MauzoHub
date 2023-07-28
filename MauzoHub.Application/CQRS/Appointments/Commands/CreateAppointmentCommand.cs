2d66e3fa0b3574d2cd8358c06d188109aa8dd3fb
Billy <bimganda@gmail.com>
2023-07-28 21:30:33 +0300

    create command class to create an appointment

---
+++ MauzoHub.Application/CQRS/Appointments/Commands/CreateAppointmentCommand.cs
@@ -0,0 +1,18 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Appointments.Commands
+{
+    public class CreateAppointmentCommand : IRequest<GetAppointmentDto>
+    {
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public Guid ServiceId { get; set; }
+        [Required]
+        public DateOnly AppointmentDate { get; set; }
+        [Required]
+        public TimeOnly AppointmentTime { get; set; }
+    }
+}
