7c10080b9361c69c1fdf1c594e3802733305bf55
Billy <bimganda@gmail.com>
2023-07-31 20:46:34 +0300

    add command class to delete an appointment

---
+++ MauzoHub.Application/CQRS/Appointments/Commands/DeleteAppointmentCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Appointments.Commands
+{
+    public class DeleteAppointmentCommand : IRequest<Unit>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
