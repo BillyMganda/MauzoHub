f4fbdc22df75dda6ae25a18686995c71b1d0178c
Billy <bimganda@gmail.com>
2023-07-31 20:54:06 +0300

    create command class to update an appointment

---
+++ MauzoHub.Application/CQRS/Appointments/Commands/UpdateAppointmentCommand.cs
@@ -0,0 +1,14 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Appointments.Commands
+{
+    public class UpdateAppointmentCommand : IRequest<GetAppointmentDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public DateOnly AppointmentDate { get; set; }
+        public TimeOnly AppointmentTime { get; set; }
+    }
+}
