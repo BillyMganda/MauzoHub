4ac873fd6146604f0cfdac0970a21b50ade8f60e
Billy <bimganda@gmail.com>
2023-07-31 21:31:04 +0300

    add controller for managing appointments

---
+++ MauzoHub.Prentation/Controllers/AppointmentsController.cs
@@ -0,0 +1,59 @@
+﻿using MauzoHub.Application.CQRS.Appointments.Commands;
+using MauzoHub.Application.CQRS.Appointments.Queries;
+using MauzoHub.Application.CQRS.BusinessCategories.Commands;
+using MauzoHub.Application.CQRS.BusinessCategories.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauzoHub.Prentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AppointmentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAppointment([FromBody] CreateAppointmentCommand command)
+        {
+            var appointmentDto = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetAppointmentById), new { id = appointmentDto.Id }, appointmentDto);
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<IActionResult> GetAppointmentById(Guid id)
+        {
+            var query = new GetAppointmentByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAppointments()
+        {
+            var query = new GetAllAppointmentsQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateAppointment(UpdateAppointmentCommand command)
+        {
+            var appointmentDto = await _mediator.Send(command);
+
+            return Ok(appointmentDto);
+        }
+
+        [HttpDelete("id/{id}")]
+        public async Task<IActionResult> DeleteAppointment(Guid id)
+        {
+            var command = new DeleteAppointmentCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
