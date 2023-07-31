b842b1380aeaf2c2bd089c4d3a8ce8b1e1f40f6a
Billy <bimganda@gmail.com>
2023-07-31 21:15:35 +0300

    create query class to get appointment bu id

---
+++ MauzoHub.Application/CQRS/Appointments/Queries/GetAppointmentByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Appointments.Queries
+{
+    public class GetAppointmentByIdQuery : IRequest<GetAppointmentDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
