cc612ab6dc4f991c7def90e6992082e14adadd6d
Billy <bimganda@gmail.com>
2023-08-01 23:30:01 +0300

    create controller method to register a normal user

--- MauzoHub.Prentation/Controllers/UsersController.cs
+++ MauzoHub.Prentation/Controllers/UsersController.cs
@@ -31,6 +31,13 @@ namespace MauzoHub.Prentation.Controllers
             return CreatedAtAction(nameof(GetUserById), new { id = userDto.Id }, userDto);
         }
 
+        [HttpPost("user")]
+        public async Task<IActionResult> CreateNormalUser([FromBody] CreateNormalUserCommand command)
+        {
+            var userDto = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetUserById), new { id = userDto.Id }, userDto);
+        }
+
         [HttpGet("id/{id}")]
         public async Task<IActionResult> GetUserById(Guid id)
         {
