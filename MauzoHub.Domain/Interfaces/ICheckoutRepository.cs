d15914a30342ee2f39c454151d48d21d5978ec5d
Billy <bimganda@gmail.com>
2023-08-09 00:25:53 +0300

    create checkout repository interface class

---
+++ MauzoHub.Domain/Interfaces/ICheckoutRepository.cs
@@ -0,0 +1,11 @@
+﻿using MauzoHub.Domain.Entities;
+
+namespace MauzoHub.Domain.Interfaces
+{
+    public interface ICheckoutRepository
+    {
+        Task<Checkout> GetByUserIdAsync(Guid userId);
+        Task<Checkout> AddAsync(Checkout checkout);
+        Task<Checkout> UpdateAsync(Checkout checkout);
+    }
+}
