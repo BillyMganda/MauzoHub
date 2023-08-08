3b896f72595ba5e33e52acd05a14e08b1e7fd39c
Billy <bimganda@gmail.com>
2023-08-08 08:59:21 +0300

    follow DDD to create a rating and review domains

---
+++ MauzoHub.Domain/Entities/Rating.cs
@@ -0,0 +1,17 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class Rating
+    {
+        public int Value { get; }
+
+        public Rating(int value)
+        {
+            if (value < 0 || value > 5)
+            {
+                throw new ArgumentException("Rating must be between 0 and 5.", nameof(value));
+            }
+
+            Value = value;
+        }
+    }
+}
