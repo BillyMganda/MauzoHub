54164c4022ce7e592dd032ecc7224a6250819f4b
Billy <bimganda@gmail.com>
2023-08-09 00:23:37 +0300

    add shipping address entity

--- MauzoHub.Domain/Entities/Checkout.cs
+++ MauzoHub.Domain/Entities/Checkout.cs
@@ -5,6 +5,6 @@
         public Guid UserId { get; set; }
         public List<CheckoutItem> Items { get; set; } = new List<CheckoutItem>();
         public Payment Payment { get; set; } = new Payment();
-        public ShippingAddress ShippingAddress { get; set; }
+        public ShippingAddress ShippingAddress { get; set; } = new ShippingAddress();
     }
 }
---
+++ MauzoHub.Domain/Entities/ShippingAddress.cs
@@ -0,0 +1,9 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class ShippingAddress
+    {
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
