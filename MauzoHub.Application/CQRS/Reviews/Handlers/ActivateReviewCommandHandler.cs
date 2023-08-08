5bd0daa351a1e8b341f225d4d678e8c1637ab25d
Billy <bimganda@gmail.com>
2023-08-08 11:44:58 +0300

    add command  handler class to activate a review

---
+++ MauzoHub.Application/CQRS/Reviews/Handlers/ActivateReviewCommandHandler.cs
@@ -0,0 +1,78 @@
+﻿using MauzoHub.Application.CQRS.Reviews.Commands;
+using MauzoHub.Application.CustomExceptions;
+using MauzoHub.Application.DTOs;
+using MauzoHub.Domain.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace MauzoHub.Application.CQRS.Reviews.Handlers
+{
+    public class ActivateReviewCommandHandler : IRequestHandler<ActivateReviewCommand, GetReviewDto>
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ActivateReviewCommandHandler(IReviewRepository reviewRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            _reviewRepository = reviewRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<GetReviewDto> Handle(ActivateReviewCommand request, CancellationToken cancellationToken)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var remoteIpAddress = httpContext.Connection.RemoteIpAddress;
+
+            var actionUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}{httpContext.Request.Path}";
+            var httpMethod = httpContext.Request.Method;
+
+            var review = await _reviewRepository.FindReviewByIdAsync(request.Id);
+
+            if (review is null)
+            {
+                var errorLog = new ErrorLog
+                {
+                    DateTime = DateTime.Now,
+                    ErrorCode = "404",
+                    ErrorMessage = $"review with id {request.Id} not found",
+                    IPAddress = remoteIpAddress!.ToString(),
+                    ActionUrl = actionUrl,
+                    HttpMethod = httpMethod,
+                };
+
+                Log.Error("An error occurred while processing the command, Invalid request Id: {@ErrorLog}", errorLog);
+                throw new NotFoundException($"review with id {request.Id} not found");
+            }
+
+            try
+            {
+                var reviewToActivate = await _reviewRepository.ActivateReview(request.Id);
+
+                var reviewDto = new GetReviewDto
+                {
+                    Id = reviewToActivate.Id,
+                    ProductOrServiceId = reviewToActivate.ProductOrServiceId,
+                    UserId = reviewToActivate.UserId,
+                    Rating = reviewToActivate.Rating,
+                    Comment = reviewToActivate.Comment,
+                };
+
+                return reviewDto;
+            }
+            catch (Exception ex)
+            {
+                var errorLog = new ErrorLog
+                {
+                    DateTime = DateTime.Now,
+                    ErrorCode = "500",
+                    ErrorMessage = ex.Message,
+                    IPAddress = remoteIpAddress.ToString(),
+                    ActionUrl = actionUrl,
+                    HttpMethod = httpMethod,
+                };
+                Log.Error(ex, "An error occurred while processing the command: {@ErrorLog}", errorLog);
+                throw;
+            }
+        }
+    }
+}
