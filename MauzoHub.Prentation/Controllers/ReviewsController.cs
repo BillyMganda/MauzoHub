e26954f788ffe5344244239423e258d7f4de83e5
Billy <bimganda@gmail.com>
2023-08-08 11:22:09 +0300

    create controller

---
+++ MauzoHub.Prentation/Controllers/ReviewsController.cs
@@ -0,0 +1,41 @@
+﻿using MauzoHub.Application.CQRS.Reviews.Commands;
+using MauzoHub.Application.CQRS.Reviews.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauzoHub.Prentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ReviewsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReview([FromBody] CreateReviewCommand command)
+        {
+            var reviewDto = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetReviewById), new { id = reviewDto.Id }, reviewDto);
+        }
+
+        [HttpGet("id/{id}")]
+        public async Task<IActionResult> GetReviewById(Guid id)
+        {
+            var query = new GetReviewByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPut("deactivate")]
+        public async Task<IActionResult> DeactivateReview(DeactivateReviewCommand command)
+        {
+            var reviewDto = await _mediator.Send(command);
+
+            return Ok(reviewDto);
+        }
+    }
+}
