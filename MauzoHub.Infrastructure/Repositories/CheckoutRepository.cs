21c8fb9aef3776d0f9d865c6976b6eb8edc4bc99
Billy <bimganda@gmail.com>
2023-08-09 00:33:01 +0300

    implement ICheckoutRepository interface class

---
+++ MauzoHub.Infrastructure/Repositories/CheckoutRepository.cs
@@ -0,0 +1,35 @@
+﻿using MauzoHub.Domain.Entities;
+using MauzoHub.Domain.Interfaces;
+using MauzoHub.Infrastructure.Databases;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace MauzoHub.Infrastructure.Repositories
+{
+    public class CheckoutRepository : ICheckoutRepository
+    {
+        private readonly IMongoCollection<Checkout> _checkoutCollection;
+        public CheckoutRepository(IOptions<MauzoHubDatabaseSettings> databaseSettings)
+        {
+            var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+            var database = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+
+            _checkoutCollection = database.GetCollection<Checkout>(databaseSettings.Value.CheckoutCollectionName);
+        }
+
+        public Task<Checkout> AddAsync(Checkout checkout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Checkout> GetByUserIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Checkout> UpdateAsync(Checkout checkout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
