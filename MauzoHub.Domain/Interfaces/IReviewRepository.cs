cdf8cca9ee9b6761342f57652453efaedabd29b3
Billy <bimganda@gmail.com>
2023-08-08 09:03:30 +0300

    create an interface to create and deactivate a review

---
+++ MauzoHub.Domain/Interfaces/IReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using MauzoHub.Domain.Entities;
+
+namespace MauzoHub.Domain.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Review CreateReview(Guid userId, Guid productOrServiceId, int ratingValue, string comment);
+        void DeactivateReview(Guid reviewId);
+    }
+}
