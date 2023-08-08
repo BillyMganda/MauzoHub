0a79921cfbfeccb7af2e9205f512ad075d7931f3
Billy <bimganda@gmail.com>
2023-08-09 00:22:11 +0300

    add payment class

--- MauzoHub.Domain/Entities/Checkout.cs
+++ MauzoHub.Domain/Entities/Checkout.cs
@@ -4,7 +4,7 @@
     {
         public Guid UserId { get; set; }
         public List<CheckoutItem> Items { get; set; } = new List<CheckoutItem>();
-        public Payment Payment { get; set; }
+        public Payment Payment { get; set; } = new Payment();
         public ShippingAddress ShippingAddress { get; set; }
     }
 }
---
+++ MauzoHub.Domain/Entities/Payment.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class Payment
+    {
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+    }
+}
