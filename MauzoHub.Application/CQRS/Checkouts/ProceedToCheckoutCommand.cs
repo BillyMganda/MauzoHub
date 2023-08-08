00dcd4bb2f4b9b8c79e317d64581f4ffeb2d21ce
Billy <bimganda@gmail.com>
2023-08-09 01:13:57 +0300

    add cqrs command to proceed to checkout

---
+++ MauzoHub.Application/CQRS/Checkouts/ProceedToCheckoutCommand.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Domain.Entities;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Checkouts
+{
+    public class ProceedToCheckoutCommand : IRequest<Unit>
+    {
+        public Guid UserId { get; set; }
+        public Payment Payment { get; set; } = new Payment();
+        public ShippingAddress ShippingAddress { get; set; } = new ShippingAddress();
+    }
+}
