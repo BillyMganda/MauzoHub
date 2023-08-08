91d183c8bbdb49961bbaf2e7737b6de37e1a4b1e
Billy <bimganda@gmail.com>
2023-08-08 10:40:18 +0300

    create command class to deactivate a review

---
+++ MauzoHub.Application/CQRS/Reviews/Commands/DeactivateReviewCommand.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reviews.Commands
+{
+    public class DeactivateReviewCommand : IRequest<GetReviewDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
