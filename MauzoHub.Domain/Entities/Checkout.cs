5496ba43b7a8f4dfa78cec07a90fc199ff7b0f8d
Billy <bimganda@gmail.com>
2023-08-09 00:19:35 +0300

    add checkout entity

---
+++ MauzoHub.Domain/Entities/Checkout.cs
@@ -0,0 +1,10 @@
+﻿namespace MauzoHub.Domain.Entities
+{
+    public class Checkout : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public List<CheckoutItem> Items { get; set; } = new List<CheckoutItem>();
+        public Payment Payment { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+    }
+}
