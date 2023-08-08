73971d4022ad7a94560371fe3372261e352b4658
Billy <bimganda@gmail.com>
2023-08-08 11:42:00 +0300

    add command class to activate a review

---
+++ MauzoHub.Application/CQRS/Reviews/Commands/ActivateReviewCommand.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reviews.Commands
+{
+    public class ActivateReviewCommand : IRequest<GetReviewDto>
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
