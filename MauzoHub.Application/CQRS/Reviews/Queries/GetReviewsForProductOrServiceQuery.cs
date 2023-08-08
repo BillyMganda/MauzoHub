844aa1306715f91ad113cff40c3f349d494eaf3a
Billy <bimganda@gmail.com>
2023-08-08 13:04:56 +0300

    add query class to Get Reviews For Product Or Service

---
+++ MauzoHub.Application/CQRS/Reviews/Queries/GetReviewsForProductOrServiceQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reviews.Queries
+{
+    public class GetReviewsForProductOrServiceQuery : IRequest<IEnumerable<GetReviewDto>>
+    {
+        [Required]
+        public Guid ProductOrServiceId { get; set; }
+    }
+}
