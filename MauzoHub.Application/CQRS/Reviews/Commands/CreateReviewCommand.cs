88164cc6b6b9147f1ff8d9d05230eb7557cf098b
Billy <bimganda@gmail.com>
2023-08-08 10:12:14 +0300

    create command class for creating a review

---
+++ MauzoHub.Application/CQRS/Reviews/Commands/CreateReviewCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reviews.Commands
+{
+    public class CreateReviewCommand : IRequest<GetReviewDto>
+    {
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public Guid ProductOrServiceId { get; set; }
+        [Required]
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
