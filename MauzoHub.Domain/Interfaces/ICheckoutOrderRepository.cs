4ff57407e414073c0024ed0de460db3cc6834ad2
Billy <bimganda@gmail.com>
2023-08-15 13:51:49 +0300

    Create an interface for the checkout order.

---
+++ MauzoHub.Domain/Interfaces/ICheckoutOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Domain.Entities;
+
+namespace MauzoHub.Domain.Interfaces
+{
+    public interface ICheckoutOrderRepository
+    {
+        Task<IEnumerable<CheckoutOrder>> GetAllAsync();
+        Task<CheckoutOrder> GetByIdAsync(Guid orderId);
+        Task<IEnumerable<CheckoutOrder>> GetByUserIdAsync(Guid userId);
+        Task<IEnumerable<CheckoutOrder>> GetOrdersBetweenDatesAsync(DateTime startDate, DateTime endDate);
+    }
+}
