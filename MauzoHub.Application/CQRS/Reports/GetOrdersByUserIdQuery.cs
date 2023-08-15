b2c5d75067fc0b402910b76bd382ab6c3e8d9e2b
Billy <bimganda@gmail.com>
2023-08-15 17:12:40 +0300

    add a query class to get all orders for a particular user

---
+++ MauzoHub.Application/CQRS/Reports/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reports
+{
+    internal class GetOrdersByUserIdQuery : IRequest<IEnumerable<OrderDto>>
+    {
+        [Required(ErrorMessage = "variable userid is required")]
+        public Guid UserId { get; set; }
+    }
+}
