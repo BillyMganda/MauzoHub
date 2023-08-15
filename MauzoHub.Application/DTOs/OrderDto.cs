287c0ec4704b882095791fe95fc6761ef13a71ef
Billy <bimganda@gmail.com>
2023-08-15 14:14:26 +0300

    create an orders dto class

---
+++ MauzoHub.Application/DTOs/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MauzoHub.Application.DTOs
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }        
+        public Guid UserId { get; set; }
+        public List<OrderItemDto> Items { get; set; } = new List<OrderItemDto>();
+        public PaymentDto Payment { get; set; } = new PaymentDto();
+        public ShippingAddressDto ShippingAddress { get; set; } = new ShippingAddressDto();
+    }
+}
