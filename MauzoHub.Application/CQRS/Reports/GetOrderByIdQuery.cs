ae9f0b01f4fd7dbb96187eb84a52a3385fa4e7ea
Billy <bimganda@gmail.com>
2023-08-15 16:50:14 +0300

    create a query handler class to get an order by its id

--- MauzoHub.Application/CQRS/Reports/GetOrderByIdQuery.cs
+++ MauzoHub.Application/CQRS/Reports/GetOrderByIdQuery.cs
@@ -1,10 +1,12 @@
 ﻿using MauzoHub.Application.DTOs;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace MauzoHub.Application.CQRS.Reports
 {
     public class GetOrderByIdQuery : IRequest<OrderDto>
     {
+        [Required]
         public Guid OrderId { get; set; }
     }
 }
