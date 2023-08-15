cd80e2428766d2e532ab1dd94598e7d6b6b4f273
Billy <bimganda@gmail.com>
2023-08-15 14:16:40 +0300

    create payment method dto class

---
+++ MauzoHub.Application/DTOs/PaymentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MauzoHub.Application.DTOs
+{
+    public class PaymentDto
+    {
+        public string PaymentMethod { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+    }
+}
