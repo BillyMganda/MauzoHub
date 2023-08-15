744441728aa89bed093387bbe91b4429f40807bd
Billy <bimganda@gmail.com>
2023-08-15 18:10:52 +0300

    add query class to get orders by date

---
+++ MauzoHub.Application/CQRS/Reports/GetOrdersForADateQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reports
+{
+    public class GetOrdersForADateQuery : IRequest<IEnumerable<OrderDto>>
+    {
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.Today;
+    }
+}
