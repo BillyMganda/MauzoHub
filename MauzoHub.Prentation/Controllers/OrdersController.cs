0257fa638ddb3ebf80028d1aec49aa3af359a30e
Billy <bimganda@gmail.com>
2023-08-15 14:37:18 +0300

    create controller with method to get all orders

---
+++ MauzoHub.Prentation/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using MauzoHub.Application.CQRS.Reports;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauzoHub.Prentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var query = new GetAllOrdersReport();
+            var orderDtos = await _mediator.Send(query);
+            return Ok(orderDtos);
+        }
+    }
+}
