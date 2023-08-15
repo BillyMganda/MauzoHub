ee033433dd1013ee5537031e30326bfa8dc912aa
Billy <bimganda@gmail.com>
2023-08-15 15:57:58 +0300

    create a query to get an order by its id

---
+++ MauzoHub.Application/CQRS/Reports/GetOrderByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+
+namespace MauzoHub.Application.CQRS.Reports
+{
+    public class GetOrderByIdQuery : IRequest<OrderDto>
+    {
+        public Guid OrderId { get; set; }
+    }
+}
