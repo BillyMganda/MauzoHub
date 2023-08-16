218d6dc06042293b056ef6d28a412ce25f40aac1
Billy <bimganda@gmail.com>
2023-08-16 08:14:21 +0300

    create query class to get orders by product id

---
+++ MauzoHub.Application/CQRS/Reports/GetOrdersByProductIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using MauzoHub.Application.DTOs;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace MauzoHub.Application.CQRS.Reports
+{
+    public class GetOrdersByProductIdQuery : IRequest<IEnumerable<OrderDto>>
+    {
+        [Required]
+        public Guid ProductId { get; set; }
+    }
+}
