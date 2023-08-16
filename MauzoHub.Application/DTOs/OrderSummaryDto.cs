f3cabefbf85e341e1b779484d0109dda023e12d0
Billy <bimganda@gmail.com>
2023-08-16 08:31:50 +0300

    create order summary dto class

---
+++ MauzoHub.Application/DTOs/OrderSummaryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MauzoHub.Application.DTOs
+{
+    public class OrderSummaryDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
